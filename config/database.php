6eff408fe8073dd498f1f96ee164478cb4377d0e
wacm2008 <gl.star@163.com>
2019-05-07 03:06:36 +0000

    中间件

--- config/database.php
+++ config/database.php
@@ -113,7 +113,8 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        //'client' => env('REDIS_CLIENT', 'predis'),
+		'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
@@ -124,6 +125,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
+			'prefix'=>'1809a_'
         ],
 
         'cache' => [
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api/user','ApiController@userapi');
+//curl get post
+Route::get('/api/test','ApiController@test');
+Route::get('/api/test/a','ApiController@testA');
+Route::get('/api/test/b','ApiController@testB');
+Route::get('/api/test/c','ApiController@testC');
+//中间件限制10次
+Route::get('/api/mid','ApiController@mid10')->middleware('request10times');
+//注册
+Route::post('/api/reg','ApiController@register');
+//登录
+Route::post('/api/login','ApiController@login');
+//个人中心
+Route::get('/api/myuser','ApiController@myuser')->middleware(['checklogin','request10times']);
